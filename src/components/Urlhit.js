26e0a96dee14bbf5b26884faa05acadb614877b2
hamza_mehmood40@hotmail.com <hamza.mahmood@funavry.com>
2021-02-04 13:48:46 +0500

    Android link

--- src/components/Urlhit.js
+++ src/components/Urlhit.js
@@ -21,9 +21,9 @@ export default class Urlhit extends Component {
         return (
             <div style={{padding:20,width:"100%",height:'100%'}}>
                 <h3>This page is for fitnessvwork app email varification.</h3>
-                <h3>For Android app verification 
+                {/* <h3>For Android app verification 
                     <a href="#" onClick={()=>{window.location = "intent://name/"+this.state.token+"/#Intent;scheme=fitnessvwork;package=com.funavry.fitnessvworkoutexpo;end"}}>Click Here</a>
-                </h3>
+                </h3> */}
             </div>
         )
     }
--- src/components/Urlhithome.js
+++ src/components/Urlhithome.js
@@ -9,9 +9,9 @@ export default class Urlhithome extends Component {
         return (
             <div style={{padding:20,width:"100%",height:'100%'}}>
                 <h3>This page is for fitnessvwork app redirect.</h3>
-                <h3>For Android app redirect 
+                {/* <h3>For Android app redirect 
                     <a href="#" onClick={()=>{window.location = "intent://app/#Intent;scheme=fitnessvwork;package=com.funavry.fitnessvworkoutexpo;end"}}>Click Here</a>
-                </h3>
+                </h3> */}
             </div>
         )
     }
